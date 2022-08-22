ca16e3c9a0954cc17b7f0c1b8184184f6bb34f85
Arjun Krishnan <artk0090@gmail.com>
2022-08-22 12:45:45 -0700

    Update to scenevariables.cs

--- bug-on-bug-action/Assets/SceneVariables.cs
+++ bug-on-bug-action/Assets/SceneVariables.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class SceneVariables 
+public static class SceneVariables
 {
     public static int deathCount { get; set; }
     public static int state { get; set; }
+    public static string mode { get; set; }
 }

8fd9c407b2ac339dd3cd2760ecb0aa165e0c3479
stubbelj <60596337+stubbelj@users.noreply.github.com>
2022-08-22 17:09:09 -0700

    Revert "corpses n stuff"
    This reverts commit 31d92ae2b2f44df9f51efd3921fef780324b1401.

--- bug-on-bug-action/Assets/Scripts/EnemyHitBox.cs
+++ bug-on-bug-action/Assets/Scripts/EnemyHitBox.cs
@@ -4,51 +4,19 @@ using UnityEngine;
 
 public class EnemyHitBox : MonoBehaviour
 {
-    GameObject player;
-    GameObject enemy;
-
-    bool currentlyDamaging = false;
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Player");
-        enemy = GameObject.Find("Enemy");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!currentlyDamaging) {
-            StartCoroutine(DamagePlayer());
-        }
+        
     }
 
-    public IEnumerator DamagePlayer() {
-        currentlyDamaging = true;
-        if (Mathf.Abs(player.transform.position.x - enemy.transform.position.x) < 25) {
-            switch (enemy.GetComponent<Enemy>().currentAttack) {
-                case "Stomp":
-                    yield return new WaitForSeconds(0.5f);
-                    player.gameObject.GetComponent<Player>().TakeDamage(34, GameObject.Find("Enemy").transform.position);
-                    break;
-                //case "Charge":
-               //     player.gameObject.GetComponent<Player>().TakeDamage(50, GameObject.Find("Enemy").transform.position);
-                  //  break;
-                case "Impale":
-                    yield return new WaitForSeconds(0.5f);
-                    player.gameObject.GetComponent<Player>().TakeDamage(90, GameObject.Find("Enemy").transform.position);
-                    break;
-                case "GroundPound":
-                    yield return new WaitForSeconds(1.0f);
-                    player.gameObject.GetComponent<Player>().TakeDamage(90, GameObject.Find("Enemy").transform.position);
-                    break;
-
-            }
-        }
-        currentlyDamaging = false;
-    }
-
-    /*private void OnTriggerEnter2D(Collider2D other) {
+    private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player")
             switch(GameObject.Find("Player").GetComponent<Player>().currentAttack) {
                 case "Stomp":
@@ -64,5 +32,5 @@ public class EnemyHitBox : MonoBehaviour
                     other.gameObject.GetComponent<Player>().TakeDamage(90, GameObject.Find("Enemy").transform.position);
                     break;
             }
-    }*/
+    }
 }
